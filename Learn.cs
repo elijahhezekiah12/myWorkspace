c6e8e84011084dbd79ab14ac1ea067f1957cd6b5
Your Name <elijahhezekiah@Elijahs-Air.home>
2016-01-16 17:18:05 +0000

    First commit

---
+++ Learn.cs
@@ -0,0 +1,31 @@
+using System; 
+
+
+
+  class Program
+ {
+   static public void Main()
+   {
+
+     Console.WriteLine("");
+       String name = Console.ReadLine();
+        Console.WriteLine("Hello"+""+name);
+
+   /**data types
+
+   bool canVote = true;
+    
+  char grade = 'A';
+
+  double maxDouble = double.MaxValue;
+
+ var anothername = "Tom";
+
+  **/
+ 
+
+  }
+
+   }
+
+ 
